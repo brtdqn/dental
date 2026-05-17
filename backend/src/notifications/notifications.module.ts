72ee818977f25f23b4864b5f2cfa4cc30893cf6a
Berat <berat@displatform.local>
2026-05-17 20:28:42 +0300

    fix: import JwtModule into NotificationsModule

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -2,8 +2,11 @@ import { Module, Global } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import NotificationsGateway from './notifications.gateway';
 
+import { JwtModule } from '@nestjs/jwt';
+
 @Global()
 @Module({
+  imports: [JwtModule],
   providers: [NotificationsService, NotificationsGateway],
   exports: [NotificationsService, NotificationsGateway],
 })
